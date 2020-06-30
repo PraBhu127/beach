44d04bd7ad920117ebf1afa8830b11dcc1392d53
PraBhu127 <pmprabhu127@gmail.com>
2020-06-30 16:12:49 +0545

    react

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
+
+export default class FeaturedRooms extends Component {
+  render() {
+    return <div>hello</div>;
+  }
+}
---
+++ src/pages/SingleRooms.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class SingleRooms extends Component {
+  render() {
+    return <div>hello from react single room page</div>;
+  }
+}
